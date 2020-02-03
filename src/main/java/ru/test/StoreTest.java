3a4fffaa4b5a1bc1acc932cc602d7af24a165160
ura <ura545@mail.ru>
2020-02-03 17:30:01 +0500

    no controller

--- src/main/java/ru/test/StoreTest.java
+++ src/main/java/ru/test/StoreTest.java
@@ -11,7 +11,6 @@ import java.util.stream.Collectors;
 
 public class StoreTest extends SnmpCommand implements TestInterface {
     private IncidentEntity incident;
-    private String url_get_page = "http://10.183.116.238/cgi-bin/getTechData.php?svc=";
 
     public StoreTest(IncidentEntity incident) {
         this.incident = incident;
@@ -23,8 +22,8 @@ public class StoreTest extends SnmpCommand implements TestInterface {
         String s = incident.getTechdata();
         //System.out.println(s);
 
-        if(s.contains("[") && !s.contains("[КСА")) {
-            if((s.substring(s.lastIndexOf("[") + 1, s.lastIndexOf("]")).length() < 15)) {
+        if (s.contains("[") && !s.contains("[КСА")) {
+            if ((s.substring(s.lastIndexOf("[") + 1, s.lastIndexOf("]")).length() < 15)) {
                 viewTest.setIp_address(s.substring(s.lastIndexOf("[") + 1, s.lastIndexOf("]")));
                 setAddress("udp:" + viewTest.getIp_address() + "/161");
             }
@@ -33,7 +32,7 @@ public class StoreTest extends SnmpCommand implements TestInterface {
                 case 2:
                 case 4:
                 case 8:
-                    if(s.contains("DSL")) {
+                    if (s.contains("DSL")) {
                         try {
                             viewTest.setSlotPort(Arrays.stream(s.substring(s.lastIndexOf("]")).replace(")", "").split("-"))
                                     .skip(1)
@@ -48,7 +47,7 @@ public class StoreTest extends SnmpCommand implements TestInterface {
                                     )
                                     .collect(Collectors.toList()));
 
-                            System.out.println(getAsString(new OID(".1.3.6.1.2.1.1.1.0")));
+                          //  System.out.println(getAsString(new OID(".1.3.6.1.2.1.1.1.0")));
                             if ("ECI telecom HiFOCuS broadband access system".equals(getAsString(new OID(".1.3.6.1.2.1.1.1.0")))) {
                                 switch (getAsString(new OID(SMI + "1286.1.3.3.1.1.2.131072"))) {
                                     case "IPNI_APP_2.52.35":
@@ -66,55 +65,36 @@ public class StoreTest extends SnmpCommand implements TestInterface {
                                         System.out.println(getAsString(new OID(SMI + "1286.1.3.3.1.1.2.131072")));
                                 }
                             }
-                        } catch (IOException e) {
-                            e.printStackTrace();
                         } catch (StringIndexOutOfBoundsException e) {
                             System.out.println("    Ex-> " + s + "tech=" + incident.getTechnogyEntity().getMameTechnology());
                         } catch (ArrayIndexOutOfBoundsException e) {
                             System.out.println("Errors --->" + s);
-                        }
-                        catch (IndexOutOfBoundsException e) {
+                        } catch (IndexOutOfBoundsException e) {
                             System.out.println("Errors Index --->" + s + "|" + s.substring(s.lastIndexOf("]")).replace(")", "").split("-").length);
                         }
                     }
 
                     break;
                 case 3: // PON
-                    try {
-                        viewTest.setSlotPortOnt(Arrays.stream((new GetResultPage(url_get_page + incident.getService()).getResultTest().readLine()).split(";"))
+                        viewTest.setSlotPortOnt(Arrays.stream((new GetResultPage("http://10.183.116.238/cgi-bin/getTechData.php?svc=" + incident.getService()).getResultTest().readLine()).split(";"))
                                 .skip(4)
                                 .collect(Collectors.toList()));
-                        System.out.println(getAsString(new OID(".1.3.6.1.2.1.1.1.0")));
                         switch (getAsString(new OID(".1.3.6.1.2.1.1.1.0"))) {
                             case "ECI telecom HiFOCuS broadband access system":
                                 new TestEciOptic(viewTest, String.valueOf(incident.getService())).test();
                                 break;
                             case "Huawei Integrated Access Software":
                                 new TestOpticHuawei(viewTest).test();
-                                //status = testing.test(String.valueOf(4194304000L + 8192 * Integer.parseInt(viewTest.getPort()) + 256 * Integer.parseInt(viewTest.getSlot()) ), viewTest.getSlot(), viewTest.getOnt(), 3);  // комманды;
                                 break;
+                            default:
+                                System.out.println(getAsString(new OID(".1.3.6.1.2.1.1.1.0")));
                         }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
                     break;
                 case 5: // FTTx
-                    viewTest.setPort(Arrays.stream(s.substring( s.lastIndexOf("]") + 1).replace("-", "").trim().split(" ")).findFirst().get());
-                    try {
-                        new TestEthernetSwitch(viewTest).test();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    viewTest.setPort(Arrays.stream(s.substring(s.lastIndexOf("]") + 1).replace("-", "").trim().split(" ")).findFirst().get());
+                    new TestEthernetSwitch(viewTest).test();
                     break;
             }
-        }
-        return viewTest;
-}
-    static Boolean isNumber(String s) {
-        try{            Integer.parseInt(s.trim());        } catch (NumberFormatException e) {            return false;        }
-        return true;
+        }   return viewTest;
     }
-
-
-
 }
